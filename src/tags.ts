cd78992d8f2f0f4e42fd9c81c8641893e9a368fe
mottox2 <y.mottox2@gmail.com>
2023-04-09 19:28:27 +0900

    Create project

---
+++ src/tags.ts
@@ -0,0 +1,167 @@
+type Tag = {
+  emoji: string
+  name: string
+  description: string
+}
+
+const tags : Tag[] = [
+  {
+    emoji: '🎮',
+    name: 'ヘブンバーンズレッド',
+    description: 'key作品が好きでAB!コラボから'
+  },
+  {
+    emoji: '🎮',
+    name: 'スプラトゥーン',
+    description: 'ローラーとエクスプロッシャー'
+  },
+  {
+    emoji: '👞',
+    name: 'ジム',
+    description: '週イチで通ってます'
+  },
+  {
+    emoji: '🎮',
+    name: 'APEX Legends',
+    description: 'ゲーム実況もよく見ます'
+  },
+  {
+    emoji: '📺',
+    name: 'ラブライブ！',
+    description: 'μ’sがめちゃくちゃ好きでした'
+  },
+  {
+    emoji: '🎮',
+    name: 'プロジェクトセカイ',
+    description: '推しはまふゆとLeo/need'
+  },
+  {
+    emoji: '🎺',
+    name: 'ホルン',
+    description: '吹奏楽とオーケストラ'
+  },
+  {
+    emoji: '📺',
+    name: 'SHIROBAKO',
+    description: '仕事で迷ったときに見てます'
+  },
+  {
+    emoji: '📕',
+    name: 'ソードアート・オンライン',
+    description: '話が好きだし、茅場晶彦に憧れる…'
+  },
+  {
+    emoji: '📺',
+    name: 'ぼっち・ざ・ろっく！',
+    description: '結束バンドを無限ループ中'
+  },
+  {
+    emoji: '🎵',
+    name: '口笛',
+    description: '知ってる曲ならだいたい吹けます'
+  },
+  {
+    emoji: '📕',
+    name: 'のだめカンタービレ',
+    description: 'オーケストラを始めたきっかけ'
+  },
+  {
+    emoji: '📷',
+    name: 'AI画像生成',
+    description: 'Stable Diffusionで挑戦中'
+  },
+  {
+    emoji: '📺',
+    name: 'RTA',
+    description: 'ドラクエをよく見ます'
+  },
+  {
+    emoji: '🎵',
+    name: 'ワルキューレ',
+    description: 'マクロスの方です'
+  },
+  {
+    emoji: '📺',
+    name: 'ヴァイオレット・エヴァーガーデン',
+    description: '10話で何回も泣かされました'
+  },
+  {
+    emoji: '📺',
+    name: 'CLANNAD',
+    description: '何度も見てます'
+  },
+  {
+    emoji: '💬',
+    name: 'つくること',
+    description: 'くだらないものづくりが好き'
+  },
+  {
+    emoji: '🍰',
+    name: '甘いもの',
+    description: '特にタルトが好きです'
+  },
+  {
+    emoji: '✏️',
+    name: '技術同人誌',
+    description: 'プログラミング本の執筆'
+  },
+  {
+    emoji: '🎵',
+    name: '槇原敬之',
+    description: '曲がとても好き'
+  },
+  {
+    emoji: '👞',
+    name: '散歩',
+    description: '知らない道が好きです'
+  },
+  {
+    emoji: '🎵',
+    name: 'Kalafina',
+    description: '梶浦由記さんの曲も好き'
+  },
+  {
+    emoji: '💄',
+    name: 'スキンケア',
+    description: '清潔感を出していきたい'
+  },
+  {
+    emoji: '📺',
+    name: 'にじさんじ',
+    description: '周防サンゴ、グウェル'
+  },
+  {
+    emoji: '📕',
+    name: '天城ブリリアントパーク',
+    description: '最終巻をずっと待ってる'
+  },
+  {
+    emoji: '📺',
+    name: '異世界食堂',
+    description: '何度も見直すグルメアニメ'
+  },
+  {
+    emoji: '🎮',
+    name: 'FORTNITE',
+    description: 'ハマっていました'
+  },
+  {
+    emoji: '📕',
+    name: '妹さえいればいい。',
+    description: 'お仕事系ラノベは大好物'
+  },
+  {
+    emoji: '📕',
+    name: 'ゼロの使い魔',
+    description: '異世界転移物では一番'
+  },
+  // NEWGAME!
+  // ダンまち
+  // すかすか
+  // リズと青い鳥
+  // ヒロアカ
+  // 冴えない彼女の育てかた
+
+]
+
+export default tags
